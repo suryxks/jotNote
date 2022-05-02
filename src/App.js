c8ab13bdc9e61337a602e790f6e042bb6d24717d
Surya <123004250@sastra.ac.in>
2022-05-02 22:46:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
 import "./App.css";
 import logo from "./logo.png";
-
+import {GlobalStyles} from './GlobalStyles';
+import styled from 'styled-components'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
-    </div>
+    <AppWrapper>
+      <h1>THis is home</h1>
+      <GlobalStyles/>
+    </AppWrapper>
   );
 }
-
+const AppWrapper=styled.div`
+height: 100vh;
+width: 100%;
+background-color: #fffffe;
+margin: 0;
+`;
 export default App;
