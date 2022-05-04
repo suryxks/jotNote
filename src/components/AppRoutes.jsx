9c61863ccb2f5072e94eedfdbb68a796746b30ad
Surya <123004250@sastra.ac.in>
2022-05-04 19:31:53 +0530

    feat:added basic routes

---
+++ src/components/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import {Home,Archive,Trash,Labels,Login,SignUp} from '../pages/index';
+export const AppRoutes = () => {
+    return (
+        <RoutesWrapper>
+            <Route path='/' element={<Home />}/>
+            <Route path='/archive' element={<Archive />}/>
+            <Route path='/trash' element={<Trash />}/>
+            <Route path='/labels' element={<Labels />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/signup' element={<SignUp />}/>
+        </RoutesWrapper>
+    );
+} 
+
+const RoutesWrapper=styled(Routes)`
+    grid-area: appContent;
+`
