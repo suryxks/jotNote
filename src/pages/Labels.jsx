8e137df4dcc60431f20b6d3ed292dd554a4e1742
Surya <123004250@sastra.ac.in>
2022-05-04 19:30:52 +0530

    feat: started Home ,Labels,Trash pages

---
+++ src/pages/Labels.jsx
@@ -0,0 +1,8 @@
+import { PageContainer } from '../components';
+export const Labels = () => {
+    return (
+        <PageContainer>
+            <h1>Labels page work in progress</h1>
+        </PageContainer>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import { Home } from "./Home";
+import {Labels} from './Labels';
+import {Trash} from './Trash';
+import {Archive} from './Archive';
+import {Login} from './Auth/Login';
+import {SignUp} from './Auth/SignUp';
+
+export {Home,Labels,Trash,Archive,Login,SignUp};
