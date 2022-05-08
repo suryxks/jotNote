1b7bda70a60d5332a85c5f823eabe3c9ec7491a7
Surya <123004250@sastra.ac.in>
2022-05-08 23:18:46 +0530

    feat:implemented trash page

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,8 +1,24 @@
+import styled from 'styled-components';
 import { PageContainer } from '../components';
+import { useData } from '../contexts/DataContext';
+import { EmptyState, NotesContainer, NoteCard, PageHeader } from '../components';
 export const Archive = () => {
+    const { archiveNotes } = useData()
     return (
         <PageContainer>
-            <h1>Archive page work in progress</h1>
+            <PageHeader><HeadingLarge>Archive</HeadingLarge></PageHeader>
+            {archiveNotes.length === 0 ? (<EmptyState>
+                <h3>No notes Here</h3>
+            </EmptyState>) : (
+                <NotesContainer>
+                    {archiveNotes.map((note) => (<NoteCard note={note} />))}
+                </NotesContainer>
+            )
+            }
         </PageContainer>
+
     )
 }
+const HeadingLarge = styled.h1`
+margin-right: auto;
+`;
