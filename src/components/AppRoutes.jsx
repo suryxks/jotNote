5c7b3cc8a75cf666f4016ad676341615037dcaa1
Surya <123004250@sastra.ac.in>
2022-05-14 22:16:39 +0530

    added routes to tags page

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,19 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
-import {Home,Archive,Trash,Labels,Login,SignUp} from '../pages/index';
+import { Home, Archive, Trash, Labels, Login, SignUp } from '../pages/index';
+import { TagContent } from './TagContent';
 export const AppRoutes = () => {
     return (
         <RoutesWrapper>
-            <Route path='/' element={<Home />}/>
-            <Route path='/archive' element={<Archive />}/>
-            <Route path='/trash' element={<Trash />}/>
-            <Route path='/labels' element={<Labels />}/>
-            <Route path='/login' element={<Login />}/>
-            <Route path='/signup' element={<SignUp />}/>
+            <Route path='/' element={<Home />} />
+            <Route path='/archive' element={<Archive />} />
+            <Route path='/trash' element={<Trash />} />
+            <Route path='/labels/:labelId' element={<TagContent />} />
+            <Route path='/labels' element={<Labels />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
         </RoutesWrapper>
     );
-} 
+}
 
-const RoutesWrapper=styled(Routes)`
+const RoutesWrapper = styled(Routes)`
     grid-area: appContent;
 `
